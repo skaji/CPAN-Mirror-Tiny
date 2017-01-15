cfdcfdbc55b8b6ee7a8a601023054fb24475cc32
Shoichi Kaji <skaji@cpan.org>
2017-01-15 12:20:16 +0900

    follow upstream

--- lib/CPAN/Mirror/Tiny.pm
+++ lib/CPAN/Mirror/Tiny.pm
@@ -352,6 +352,7 @@ sub _update_packages {
       # lower VERSION number
     }
     else {
+      no warnings; # numeric/version
       if (
         $new_version eq 'undef' or $new_version == 0 or
         Parse::PMFile->_vcmp($new_version, $cur_version) == 0
